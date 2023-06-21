ef628cee6975726a04c3bce9d6cb4f84b7a54e97
Roman <121165911+madmanRE@users.noreply.github.com>
2023-06-21 20:50:00 +0300

    Разработка проекта, усовершенствование (прикрутил комменты, отправку по емейл, пагинатор)

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from django.http import Http404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, ''
-                           'blog/post/list.html',
-                  {'posts': posts})
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} рекомендует почитать {post.title}"
+            message = f"Почитай статью '{post.title}' по адресу {post_url}\n\n " \
+                      f"{cd['name']} комментирует: {cd['comments']}"
+            send_mail(subject, message, 'romangvaramadze@gmail.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
+
+
+class PostListView(ListView):
+    """Альтернативное представление списка постов"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+    def post_list(request):
+        post_list = Post.published.all()
+        paginator = Paginator(post_list, 3)
+        page_number = request.GET.get('page', 1)
+        try:
+            posts = paginator.page(page_number)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        return render(request, ''
+                               'blog/post/list.html',
+                      {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -16,7 +61,29 @@ def post_detail(request, year, month, day, post):
                              slug=post,
                              publish__year=year,
                              publish__month=month,
-                             publish__day=day,)
+                             publish__day=day, )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request,'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
--- django_news/settings.py
+++ django_news/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'romangvaramadze@gmail.com'
+EMAIL_HOST_PASSWORD = 'xnvgkiswgzoezxcj'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
