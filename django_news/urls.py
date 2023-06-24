a70036614ec2a6ae23e4d189ccc30395a55f6f20
Roman <121165911+madmanRE@users.noreply.github.com>
2023-06-24 23:54:23 +0300

    Доделал сайта Django news

--- django_news/urls.py
+++ django_news/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from blog.views import index
 
 sitemaps = {
     'posts': PostSitemap,
@@ -27,5 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('', index, name='index')
 ]
